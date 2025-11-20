8ad4ffbf18c4395c45c364efc53e0732440a6999
KeatenC <keatencavill@hotmail.com>
2025-11-20 13:49:33 -0700

    added student/personal page

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import SignUpView, SignInView, TeacherDashBoardView
+from .views import (
+    SignUpView,
+    SignInView,
+    logout_view,
+    TeacherDashBoardView,
+    StudenDashBoardView,
+    PersonalDashBoardView,
+)
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("signin/", SignInView.as_view(), name="signin"),
-    path("logout/", LogoutView.as_view(next_page="signin"), name="logout"),
-    path("teacher/", TeacherDashBoardView.as_view(), name= 'teacher_dashboard'),
+    path("logout/", logout_view, name="logout"),
+    path("teacher/", TeacherDashBoardView.as_view(), name="teacher_dashboard"),
+    path("student/", StudenDashBoardView.as_view(), name="student_dashboard"),
+    path("personal/", PersonalDashBoardView.as_view(), name="personal_dashboard"),
 ]
