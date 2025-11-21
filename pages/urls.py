4e910aa216cda610fe790b2be47e6ac164941e92
Nathan Dunifer <hailomalas@gmail.com>
2025-11-20 21:51:48 -0700

    Reset database, rebuild migrations, and integrate latest updates

--- pages/urls.py
+++ pages/urls.py
@@ -1,18 +1,9 @@
 from django.urls import path
-<<<<<<< Updated upstream
-from .views import HomePage, ZshTerminal, BashTerminal, PowershellTerminal
-=======
 from .views import HomePage, ZshTerminal, BashTerminal, PowerShell
->>>>>>> Stashed changes
 
 urlpatterns = [
     path("", HomePage.as_view(), name="home"),
     path('zsh-terminal/', ZshTerminal.as_view(), name='zsh_terminal'),
     path('bash-terminal/', BashTerminal.as_view(), name='bash_terminal'),
-<<<<<<< Updated upstream
-    path('powershell-terminal/', PowershellTerminal.as_view(), name='powershell_terminal'),
-]
-=======
     path('powerShell-terminal/', PowerShell.as_view(), name='powerShell_terminal'),
 ]
->>>>>>> Stashed changes
