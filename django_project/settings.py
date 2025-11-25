9dff391997f5bee712d9c430c6374d70bf178a5b
joserod0704 <jose1.rodriguez@umconnect.umt.edu>
2025-11-24 21:22:50 -0700

    edited home page

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "account.CustomUser"
 
-# STATIC_URL = "/static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "staticfiles",
